e32170951c80055ac3d7defd352d70f54f9b6671
Peter Larsson <peter.larsson@callistaenterprise.se>
2014-04-22 14:27:22 +0200

    Minor change, use @Beaan insteand of @ComponentScan (just to demonstrate)

--- util/src/main/java/com/patrix/util/config/UtilConfiguration.java
+++ util/src/main/java/com/patrix/util/config/UtilConfiguration.java
@@ -1,12 +1,26 @@
 package com.patrix.util.config;
 
+import com.patrix.util.UtilService;
+import com.patrix.util.impl.TestUserServiceImpl;
+import com.patrix.util.impl.UtilServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * Created by peter on 2014-03-18.
  */
 @Configuration
-@ComponentScan(basePackages = { "com.patrix.util.impl" })
 public class UtilConfiguration {
+
+    @Bean
+    public UtilService utilService() {
+        return new UtilServiceImpl();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new TestUserServiceImpl();
+    }
 }
